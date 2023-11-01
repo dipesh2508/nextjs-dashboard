7e99474b2a07de4abb823fe38bc3eb5456b03aa2
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-11-01 13:41:58 +0530

    added home module css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from './ui/home.module.css';
 
 import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <div className={styles.shape}></div>;
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
